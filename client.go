7c2e41453122d3cba0fcb7e3c25996a125d87efd
Muir Manders <muir.manders@gmail.com>
2015-02-13 21:39:06 -0800

    Implement Retrieve
    Now you can fetch stuff!
    
    Also fix retrieve and namelist to put the connection back in the pool.

--- client.go
+++ client.go
@@ -10,7 +10,6 @@ import (
 	"io"
 	"net"
 	"net/textproto"
-	"strconv"
 	"strings"
 	"sync"
 	"sync/atomic"
@@ -41,42 +40,6 @@ type Config struct {
 
 	// Logging destination for debugging messages. Set to os.Stderr to log to stderr.
 	Logger io.Writer
-}
-
-// Represents a single connection to an FTP server.
-type persistentConn struct {
-	// control socket
-	controlConn net.Conn
-
-	// control socket read/write helpers
-	reader *textproto.Reader
-	writer *textproto.Writer
-
-	// has this connection encountered an unrecoverable error
-	broken bool
-
-	// index from 1..MaxConnections (used for logging context)
-	idx int32
-}
-
-func (pconn *persistentConn) close() {
-	if pconn.controlConn != nil {
-		pconn.controlConn.Close()
-	}
-}
-
-func (pconn *persistentConn) sendCommand(f string, args ...interface{}) (int, string, error) {
-	err := pconn.writer.PrintfLine(f, args...)
-	if err != nil {
-		return 0, "", fmt.Errorf("error writing command: %s", err)
-	}
-
-	code, msg, err := pconn.reader.ReadResponse(0)
-	if err != nil {
-		return 0, "", fmt.Errorf("error reading response: %s")
-	}
-
-	return code, msg, err
 }
 
 // Construct and return a new client Conn
@@ -123,15 +86,17 @@ type client struct {
 	t0           time.Time
 }
 
-// pass along error so you can chain to return
 func (c *client) debug(f string, args ...interface{}) {
 	if c.config.Logger == nil {
 		return
 	}
 
-	delta := fmt.Sprintf("%.3f ", time.Now().Sub(c.t0).Seconds())
+	msg := fmt.Sprintf("goftp: %.3f %s\n",
+		time.Now().Sub(c.t0).Seconds(),
+		fmt.Sprintf(f, args...),
+	)
 
-	c.config.Logger.Write([]byte(fmt.Sprintf("goftp: "+delta+f+"\n", args...)))
+	c.config.Logger.Write([]byte(msg))
 }
 
 func (c *client) getIdleConn() (*persistentConn, error) {
@@ -142,11 +107,11 @@ Loop:
 		select {
 		case pconn := <-c.freeConnCh:
 			if pconn.broken {
-				c.debug("connection %d was waiting (broken)", pconn.idx)
+				c.debug("#%d was ready (broken)", pconn.idx)
 				atomic.AddInt32(&c.numOpenConns, -1)
 				pconn.close()
 			} else {
-				c.debug("connection %d was waiting", pconn.idx)
+				c.debug("#%d was ready", pconn.idx)
 				return pconn, nil
 			}
 		default:
@@ -164,7 +129,7 @@ Loop:
 			c.mu.Unlock()
 			pconn, err := c.openConn(idx)
 			if err != nil {
-				c.debug("connection %d error connecting: %s", idx, err)
+				c.debug("#%d error connecting: %s", idx, err)
 				atomic.AddInt32(&c.numOpenConns, -1)
 			}
 			return pconn, err
@@ -175,11 +140,11 @@ Loop:
 			pconn := <-c.freeConnCh
 
 			if pconn.broken {
-				c.debug("waited for connection %d (broken)", pconn.idx)
+				c.debug("waited for #%d (broken)", pconn.idx)
 				atomic.AddInt32(&c.numOpenConns, -1)
 				pconn.close()
 			} else {
-				c.debug("waited for connection %d", pconn.idx)
+				c.debug("waited for #%d", pconn.idx)
 				return pconn, nil
 			}
 		}
@@ -188,7 +153,7 @@ Loop:
 
 // open control connection and log in if appropriate
 func (c *client) openConn(idx int32) (pconn *persistentConn, err error) {
-	c.debug("connection %d dialing %s", idx, c.config.Host)
+	c.debug("opening #%d to %s", idx, c.config.Host)
 	conn, err := net.DialTimeout("tcp", c.config.Host, c.config.Timeout)
 	if err != nil {
 		return nil, err
@@ -199,6 +164,9 @@ func (c *client) openConn(idx int32) (pconn *persistentConn, err error) {
 		idx:         idx,
 		reader:      textproto.NewReader(bufio.NewReader(conn)),
 		writer:      textproto.NewWriter(bufio.NewWriter(conn)),
+		features:    make(map[string]string),
+		config:      c.config,
+		t0:          c.t0,
 	}
 
 	_, _, err = pconn.reader.ReadResponse(ReplyServiceReady)
@@ -206,11 +174,13 @@ func (c *client) openConn(idx int32) (pconn *persistentConn, err error) {
 		goto Error
 	}
 
-	if err = c.logInConn(pconn); err != nil {
+	if err = pconn.logInConn(); err != nil {
 		goto Error
 	}
 
-	c.debug("connection %d finished setup", idx)
+	if err = pconn.fetchFeatures(); err != nil {
+		goto Error
+	}
 
 	return pconn, nil
 
@@ -219,94 +189,62 @@ Error:
 	return nil, err
 }
 
-func (c *client) logInConn(pconn *persistentConn) error {
-	if c.config.User == "" {
-		return nil
-	}
-
-	c.debug("connection %d logging in as user %s", pconn.idx, c.config.User)
-	code, msg, err := pconn.sendCommand("USER %s", c.config.User)
+func (c *client) Retrieve(path string, dest io.Writer) error {
+	pconn, err := c.getIdleConn()
 	if err != nil {
 		return err
 	}
 
-	if code == ReplyNeedPassword {
-		c.debug("connection %d sending password", pconn.idx)
-		code, msg, err = pconn.sendCommand("PASS %s", c.config.Password)
-		if err != nil {
-			return err
-		}
+	defer func() {
+		c.freeConnCh <- pconn
+	}()
+
+	if err = pconn.setType("I"); err != nil {
+		return err
+	}
+
+	dc, err := pconn.openDataConn()
+	if err != nil {
+		pconn.debug("error opening data connection: %s", err)
+		return err
+	}
+
+	// to catch early returns
+	defer dc.Close()
+
+	pconn.debug("sending RETR %s", path)
+	code, msg, err := pconn.sendCommand("RETR %s", path)
+	if err != nil {
+		return err
+	}
+
+	if !positivePreliminaryReply(code) {
+		pconn.debug("unexpected response: %d (%s)", code, msg)
+		return fmt.Errorf("unexpected response: %d (%s)", code, msg)
+	}
+
+	_, err = io.Copy(dest, dc)
+
+	if err != nil {
+		return err
+	}
+
+	err = dc.Close()
+	if err != nil {
+		pconn.debug("error closing data connection: %s", err)
+	}
+
+	code, msg, err = pconn.reader.ReadResponse(0)
+	if err != nil {
+		pconn.debug("error reading response: %s", err)
+		return err
 	}
 
 	if !positiveCompletionReply(code) {
-		return fmt.Errorf("unexpected response: %d (%s)", code, msg)
+		pconn.debug("unexpected result: %d (%s)", code, msg)
+		return fmt.Errorf("unexpected result: %d (%s)", code, msg)
 	}
 
-	return nil
-}
-
-func (c *client) openDataConn(pconn *persistentConn) (net.Conn, error) {
-	c.debug("connection %d requesting PASV mode", pconn.idx)
-	code, msg, err := pconn.sendCommand("PASV")
-	if err != nil {
-		return nil, err
-	}
-
-	if code != ReplyEnteringPassiveMode {
-		return nil, fmt.Errorf("server doesn't support passive mode (%d %s)", code, msg)
-	}
-
-	parseError := fmt.Errorf("error parsing PASV response (%s)", msg)
-
-	// "Entering Passive Mode (162,138,208,11,223,57)."
-	openParen := strings.Index(msg, "(")
-	if openParen == -1 {
-		return nil, parseError
-	}
-
-	closeParen := strings.LastIndex(msg, ")")
-	if closeParen == -1 || closeParen < openParen {
-		return nil, parseError
-	}
-
-	addrParts := strings.Split(msg[openParen+1:closeParen], ",")
-	if len(addrParts) != 6 {
-	}
-
-	ip := net.ParseIP(strings.Join(addrParts[0:4], "."))
-	if ip == nil {
-		return nil, parseError
-	}
-
-	port := 0
-	for i, part := range addrParts[4:6] {
-		portOctet, err := strconv.Atoi(part)
-		if err != nil {
-			return nil, parseError
-		}
-		port |= portOctet << (byte(1-i) * 8)
-	}
-
-	c.debug("connection %d opening data connection to %s:%d", pconn.idx, ip.String(), port)
-
-	return net.DialTimeout("tcp", fmt.Sprintf("%s:%d", ip.String(), port), c.config.Timeout)
-}
-
-func (c *client) typeAscii(pconn *persistentConn) error {
-	c.debug("connection %d switching to ASCII mode", pconn.idx)
-	code, msg, err := pconn.sendCommand("TYPE A")
-	if err != nil {
-		return err
-	}
-
-	if code != ReplyCommandOkay {
-		return fmt.Errorf("unexpected response: %d (%s)", code, msg)
-	}
-
-	return nil
-}
-
-func (c *client) Retrieve(path string, dest io.Writer) error {
 	return nil
 }
 
@@ -316,23 +254,27 @@ func (c *client) NameList(path string) ([]string, error) {
 		return nil, err
 	}
 
-	c.debug("connection %d performing NameList", pconn.idx)
+	defer func() {
+		c.freeConnCh <- pconn
+	}()
 
-	dc, err := c.openDataConn(pconn)
+	dc, err := pconn.openDataConn()
 	if err != nil {
-		c.debug("connection %d error opening data connection: %s", pconn.idx, err)
+		pconn.debug("error opening data connection: %s", err)
 		return nil, err
 	}
 
-	c.debug("connection %d sending NLST %s", pconn.idx, path)
+	// to catch early returns
+	defer dc.Close()
 
+	pconn.debug("sending NLST")
 	code, msg, err := pconn.sendCommand("NLST %s", path)
 	if err != nil {
 		return nil, err
 	}
 
 	if !positivePreliminaryReply(code) {
-		c.debug("connection %d unexpected response: %d (%s)", pconn.idx, code, msg)
+		pconn.debug("unexpected response: %d (%s)", code, msg)
 		return nil, fmt.Errorf("unexpected response: %d (%s)", code, msg)
 	}
 
@@ -346,25 +288,27 @@ func (c *client) NameList(path string) ([]string, error) {
 
 	var dataError error
 	if err = scanner.Err(); err != nil {
-		c.debug("connection %d error reading NLST data: %s", err)
+		pconn.debug("error reading NLST data: %s", err)
 		dataError = fmt.Errorf("error reading NLST data: %s", err)
 	}
 
-	c.debug("connection %d closing data connection", pconn.idx)
-	dc.Close()
+	err = dc.Close()
+	if err != nil {
+		pconn.debug("error closing data connection: %s", err)
+	}
 
 	code, msg, err = pconn.reader.ReadResponse(0)
 	if err != nil {
-		c.debug("connection %d error reading response: %s", err)
+		pconn.debug("error reading response: %s", err)
 		return nil, err
 	}
 
 	if !positiveCompletionReply(code) {
-		c.debug("connection %d unexpected result: %d (%s)", pconn.idx, code, msg)
-		return nil, fmt.Errorf("%d unexpected result: %d (%s)", pconn.idx, code, msg)
+		pconn.debug("unexpected result: %d (%s)", code, msg)
+		return nil, fmt.Errorf("unexpected result: %d (%s)", code, msg)
 	}
 
-	c.debug("connection %d finished NameList (%d %s)", pconn.idx, code, msg)
+	pconn.debug("finished NameList (%d %s)", code, msg)
 
 	if dataError != nil {
 		return nil, dataError
---
+++ persistent_connection.go
@@ -0,0 +1,186 @@
+// Copyright 2015 Muir Manders.  All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package goftp
+
+import (
+	"fmt"
+	"net"
+	"net/textproto"
+	"strconv"
+	"strings"
+	"time"
+)
+
+// Represents a single connection to an FTP server.
+type persistentConn struct {
+	// control socket
+	controlConn net.Conn
+
+	// control socket read/write helpers
+	reader *textproto.Reader
+	writer *textproto.Writer
+
+	config Config
+	t0     time.Time
+
+	// has this connection encountered an unrecoverable error
+	broken bool
+
+	// index from 1..MaxConnections (used for logging context)
+	idx int32
+
+	// map of ftp features available on server
+	features map[string]string
+}
+
+func (pconn *persistentConn) close() {
+	pconn.debug("closed")
+	if pconn.controlConn != nil {
+		pconn.controlConn.Close()
+	}
+}
+
+func (pconn *persistentConn) sendCommand(f string, args ...interface{}) (int, string, error) {
+	err := pconn.writer.PrintfLine(f, args...)
+	if err != nil {
+		return 0, "", fmt.Errorf("error writing command: %s", err)
+	}
+
+	code, msg, err := pconn.reader.ReadResponse(0)
+	if err != nil {
+		return 0, "", fmt.Errorf("error reading response: %s")
+	}
+
+	return code, msg, err
+}
+
+func (pconn *persistentConn) debug(f string, args ...interface{}) {
+	if pconn.config.Logger == nil {
+		return
+	}
+
+	msg := fmt.Sprintf("goftp: #%d %.3f %s\n",
+		pconn.idx,
+		time.Now().Sub(pconn.t0).Seconds(),
+		fmt.Sprintf(f, args...),
+	)
+
+	pconn.config.Logger.Write([]byte(msg))
+}
+
+func (pconn *persistentConn) fetchFeatures() error {
+	pconn.debug("fetching features")
+
+	code, msg, err := pconn.sendCommand("FEAT")
+	if err != nil {
+		return fmt.Errorf("error fetching features: %s", err)
+	}
+
+	if !positiveCompletionReply(code) {
+		pconn.debug("server doesn't support FEAT: %d (%s)", code, msg)
+		return nil
+	}
+
+	for _, line := range strings.Split(msg, "\n") {
+		if line[0] == ' ' {
+			parts := strings.SplitN(strings.TrimSpace(line), " ", 2)
+			if len(parts) == 1 {
+				pconn.features[parts[0]] = ""
+			} else if len(parts) == 2 {
+				pconn.features[parts[0]] = parts[1]
+			}
+		}
+	}
+
+	return nil
+}
+
+func (pconn *persistentConn) logInConn() error {
+	if pconn.config.User == "" {
+		return nil
+	}
+
+	pconn.debug("logging in as user %s", pconn.config.User)
+	code, msg, err := pconn.sendCommand("USER %s", pconn.config.User)
+	if err != nil {
+		return err
+	}
+
+	if code == ReplyNeedPassword {
+		pconn.debug("sending password")
+		code, msg, err = pconn.sendCommand("PASS %s", pconn.config.Password)
+		if err != nil {
+			return err
+		}
+	}
+
+	if !positiveCompletionReply(code) {
+		return fmt.Errorf("unexpected response: %d (%s)", code, msg)
+	}
+
+	return nil
+}
+
+func (pconn *persistentConn) openDataConn() (net.Conn, error) {
+	pconn.debug("requesting PASV mode")
+	code, msg, err := pconn.sendCommand("PASV")
+	if err != nil {
+		return nil, err
+	}
+
+	if code != ReplyEnteringPassiveMode {
+		return nil, fmt.Errorf("server doesn't support passive mode (%d %s)", code, msg)
+	}
+
+	parseError := fmt.Errorf("error parsing PASV response (%s)", msg)
+
+	// "Entering Passive Mode (162,138,208,11,223,57)."
+	openParen := strings.Index(msg, "(")
+	if openParen == -1 {
+		return nil, parseError
+	}
+
+	closeParen := strings.LastIndex(msg, ")")
+	if closeParen == -1 || closeParen < openParen {
+		return nil, parseError
+	}
+
+	addrParts := strings.Split(msg[openParen+1:closeParen], ",")
+	if len(addrParts) != 6 {
+		return nil, parseError
+	}
+
+	ip := net.ParseIP(strings.Join(addrParts[0:4], "."))
+	if ip == nil {
+		return nil, parseError
+	}
+
+	port := 0
+	for i, part := range addrParts[4:6] {
+		portOctet, err := strconv.Atoi(part)
+		if err != nil {
+			return nil, parseError
+		}
+		port |= portOctet << (byte(1-i) * 8)
+	}
+
+	pconn.debug("opening data connection to %s:%d", ip.String(), port)
+
+	return net.DialTimeout("tcp", fmt.Sprintf("%s:%d", ip.String(), port), pconn.config.Timeout)
+}
+
+func (pconn *persistentConn) setType(t string) error {
+	pconn.debug("switching to TYPE %s", t)
+	code, msg, err := pconn.sendCommand("TYPE %s", t)
+	if err != nil {
+		return err
+	}
+
+	if code != ReplyCommandOkay {
+		return fmt.Errorf("unexpected response: %d (%s)", code, msg)
+	}
+
+	return nil
+}
