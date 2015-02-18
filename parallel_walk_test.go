25c3db553741a3600cc3e8d53136352f29ccb5f8
Muir Manders <muir.manders@gmail.com>
2015-02-17 21:27:10 -0800

    Add an experimental parallel walk example.
    Give an example of the fun kind of thing you can do with an FTP
    connection pool.

---
+++ parallel_walk_test.go
@@ -0,0 +1,83 @@
+// Copyright 2015 Muir Manders.  All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package goftp
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"sync/atomic"
+)
+
+// Just for fun, walk an ftp server in parallel. I make no claim that this is
+// correct or a good idea.
+func ExampleClient_ReadDir_parallelWalk() {
+	client, err := Dial("ftp.hq.nasa.gov")
+	if err != nil {
+		panic(err)
+	}
+
+	Walk(client, "", func(dir string, info os.FileInfo, err error) error {
+		if err != nil {
+			// no permissions is okay, keep walking
+			if strings.Contains(err.Error(), "550") {
+				return nil
+			}
+			return err
+		}
+
+		fmt.Println(path.Join(dir, info.Name()))
+
+		return nil
+	})
+}
+
+// Walk a FTP file tree in parallel with prunability and error handling.
+// See http://golang.org/pkg/path/filepath/#Walk for interface details.
+func Walk(client *Client, root string, walkFn filepath.WalkFunc) (ret error) {
+	dirsToCheck := make(chan string, 100)
+
+	var workCount int32 = 1
+	dirsToCheck <- root
+
+	for dir := range dirsToCheck {
+		go func(dir string) {
+			files, err := client.ReadDir(dir)
+
+			if err != nil {
+				if err = walkFn(dir, nil, err); err != nil && err != filepath.SkipDir {
+					ret = err
+					close(dirsToCheck)
+					return
+				}
+			}
+
+			for _, file := range files {
+				if err = walkFn(dir, file, nil); err != nil {
+					if file.IsDir() && err == filepath.SkipDir {
+						continue
+					}
+					ret = err
+					close(dirsToCheck)
+					return
+				}
+
+				if file.IsDir() {
+					atomic.AddInt32(&workCount, 1)
+					dirsToCheck <- path.Join(dir, file.Name())
+				}
+			}
+
+			atomic.AddInt32(&workCount, -1)
+			if workCount == 0 {
+				close(dirsToCheck)
+			}
+		}(dir)
+	}
+
+	return ret
+}
